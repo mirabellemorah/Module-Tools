b4257b33332a4324a95128f0d87fafaac1e1a391
mirabellemorah <myrakuti@gmail.com>
2026-07-05 13:18:36 +0100

    Corrected name

--- individual-shell-tools/awk/script-06-stretch.sh
+++ individual-shell-tools/awk/script-06-stretch.sh
@@ -7,4 +7,4 @@ set -euo pipefail
 # TODO: Write a command to output the total of adding together all players' first scores.
 # Your output should be exactly the number 54.
 
-awk '{sum += $3} END {print "the total first core is " sum}' scores-table.txt
+awk '{sum += $3} END {print "the total first score is " sum}' scores-table.txt
