00867a49bd7a0f2224033f355c60866682c8e589
mirabellemorah <myrakuti@gmail.com>
2026-07-05 09:39:51 +0100

    grep done

--- individual-shell-tools/grep/script-03.sh
+++ individual-shell-tools/grep/script-03.sh
@@ -4,3 +4,5 @@ set -euo pipefail
 
 # TODO: Write a command to output the number of lines in dialogue.txt that contain the word Doctor (regardless of case).
 # The output should be exactly the number 9.
+
+grep -c -i Doctor dialogue.txt
