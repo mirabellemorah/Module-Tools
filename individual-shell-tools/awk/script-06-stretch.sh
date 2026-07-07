b5f970c789d096df4b8ac2361ccdf7a8093f2072
mirabellemorah <myrakuti@gmail.com>
2026-07-07 15:32:21 +0100

    Adjusted AWK script-06-stretch.sh and and Sed

--- individual-shell-tools/awk/script-06-stretch.sh
+++ individual-shell-tools/awk/script-06-stretch.sh
@@ -7,4 +7,6 @@ set -euo pipefail
 # TODO: Write a command to output the total of adding together all players' first scores.
 # Your output should be exactly the number 54.
 
-awk '{sum += $3} END {print "the total first score is " sum}' scores-table.txt
+# awk '{sum += $3} END {print "the total first score is " sum}' scores-table.txt
+
+awk '{sum += $3} END {print sum}' scores-table.txt
