39a4f90f7363bf24fcdf2d340e3e3540fd3521b9
mirabellemorah <myrakuti@gmail.com>
2026-07-21 20:53:50 +0100

    Completed sort

--- shell-pipelines/sort-uniq-head-tail/script-04.sh
+++ shell-pipelines/sort-uniq-head-tail/script-04.sh
@@ -5,3 +5,5 @@ set -euo pipefail
 # The input for this script is the scores-table.txt file.
 # TODO: Write a command to output scores-table.txt, with shows the line for the player whose first score was the second highest.
 # Your output should be: "Piotr Glasgow 15 2 25 11 8" (without quotes).
+
+sort -n -r -k3 scores-table.txt | tail -n5 | head -n1
