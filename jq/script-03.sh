e2b04224aef61cc0c84140952d6712a23517faa4
mirabellemorah <myrakuti@gmail.com>
2026-07-21 18:57:10 +0100

    Up to 6 done

--- jq/script-03.sh
+++ jq/script-03.sh
@@ -6,4 +6,4 @@ set -euo pipefail
 # TODO: Write a command to output the name of the person, then a comma, then their profession.
 # Your output should be exactly the string "Selma, Software Engineer", but should not contain any quote characters.
 
-jq -r -j '.name .profession' person.json
+jq -r '[.name, .profession] | join(", ")' person.json
